ef43df6f1dc4965e73cfd668e8b7ce9fa92818b9
Jack Whitham <jack.d.whitham@gmail.com>
2015-06-15 01:48:08 +0100

    some basic redirection of syscalls

--- loader.c
+++ loader.c
@@ -7,22 +7,84 @@
 #include <unistd.h>
 #include <stdint.h>
 #include <string.h>
+#include <fcntl.h>
+#include <time.h>
 
-int debugging_printf = 1;
+#include "uClibc-0.9.33.2/libc/termios/kernel_termios.h"
+
+#define max_heap_size 0x100000
+#define max_fd_handles 0x10
+
+static int debugging_printf = 1;
 static uint64_t break_start, break_end;
-static const uint64_t break_size = 0x100000;
+static FILE * fd_handle[max_fd_handles];
 
 int64_t syscall_handler (int64_t syscall_number,
         int64_t p0, int64_t p1, int64_t p2, int64_t p3, int64_t p4)
 {
+    struct __kernel_termios * k;
+    unsigned i;
+
     switch (syscall_number) {
         case 0x10:
-            if (debugging_printf) {
-                printf ("ioctl fd1 %jx cmd %jx ptr %jx\n", p0, p1, p2);
+            /* ioctl */
+            switch (p1) {
+                case 0x5401:
+                    /* TCGETS */
+                    k = (struct __kernel_termios *) p2;
+                    memset (k, 0, sizeof (struct __kernel_termios));
+                    return 0;
+                default:
+                    printf ("Unsupported ioctl %ju p1 %jx p2 %jx\n",
+                            p0, p1, p2);
+                    exit (1);
+                    return 0;
             }
-            return ioctl (p0, p1, p2, p3, p4);
+        case 2:
+            if ((p1 & O_WRONLY) || (p1 & O_RDWR)) {
+                printf ("Unsupported open mode for '%s': 0x%jx\n",
+                        (const char *) p0, p1);
+                exit (1);
+            } else {
+                FILE * fd = fopen ((const char *) p0, "rb");
+                if (fd == NULL) {
+                    /* file not found, perhaps */
+                    return -1;
+                }
+                for (i = 0; i < max_fd_handles; i++) {
+                    if (fd_handle[i] == NULL) {
+                        fd_handle[i] = fd;
+                        fd = NULL;
+                        return i;
+                    }
+                }
+                printf ("No handles available for '%s'\n",
+                        (const char *) p0);
+                exit (1);
+            }
+        case 0:
         case 1:
-            return write (p0, (const void *) p1, p2);
+        case 3:
+            if ((p0 < 0) || (p0 >= (int64_t) max_fd_handles)) {
+                printf ("Invalid handle: 0x%jx\n", p0);
+                exit (1);
+            }
+            switch (syscall_number) {
+                case 0:
+                    fflush (stdout);
+                    p0 = fread ((void *) p1, 1, p2, fd_handle[p0]);
+                    break;
+                case 1:
+                    p0 = fwrite ((void *) p1, 1, p2, fd_handle[p0]);
+                    fflush (fd_handle[p0]);
+                    break;
+                default:
+                    fclose (fd_handle[p0]);
+                    fd_handle[p0] = NULL;
+                    p0 = 0;
+                    break;
+            }
+            return p0;
         case 60:
             if (debugging_printf) {
                 printf ("exit\n");
@@ -40,8 +102,11 @@ int64_t syscall_handler (int64_t syscall_number,
                 printf ("brk %016jx (%ju bytes)\n", p0, p0 - break_start);
             }
             return p0;
+        case 201:
+            /* time */
+            return 1400000000;
         default:
-            printf ("Unsupported system call %jx p0 %jx p1 %jx p2 %jx\n",
+            printf ("Unsupported system call %ju p0 %jx p1 %jx p2 %jx\n",
                     syscall_number, p0, p1, p2);
             exit (1);
             return 0;
@@ -67,9 +132,10 @@ int main (int argc, char ** argv)
     uint64_t        end_address;
     int             rc;
     uint64_t        i;
-    struct termios  ignore;
 
-    tcgetattr (0, &ignore);
+    fd_handle[0] = stdin;
+    fd_handle[1] = stdout;
+    fd_handle[2] = stderr;
 
     printf ("argc = %d\n", argc);
     printf ("argv = %p\n", argv);
@@ -123,14 +189,14 @@ int main (int argc, char ** argv)
         return 1;
     }
 
-    p0 = mmap (NULL, end_address + break_size,
+    p0 = mmap (NULL, end_address + max_heap_size,
               PROT_READ | PROT_WRITE | PROT_EXEC,
               MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
     assert (p0 != NULL);
     printf ("p0 = %p\n", p0);
 
     break_start = ((uint64_t) p0) + end_address;
-    break_end = ((uint64_t) p0) + end_address + break_size;
+    break_end = ((uint64_t) p0) + end_address + max_heap_size;
 
     memset (p0, 0, end_address);
     memcpy (p0, p1, load_size);
